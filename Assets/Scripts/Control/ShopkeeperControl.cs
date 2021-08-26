9afd26694ebb82fe15b4412df0bde49d5c284dbe
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2021-08-26 17:51:41 -0300

    first sell

--- Assets/Scripts/Control/ShopkeeperControl.cs
+++ Assets/Scripts/Control/ShopkeeperControl.cs
@@ -3,12 +3,30 @@ using SIM.Core;
 
 namespace SIM.Control
 {
+    [RequireComponent(typeof(Trader))]
     public class ShopkeeperControl : MonoBehaviour, IInteractable
     {
+        Trader trader;
+
+        private void Awake()
+        {
+            trader = GetComponent<Trader>();
+        }
+
         public void Interact(GameObject whoInteracts, out GameObject interacted)
         {
             interacted = gameObject;
-            print(whoInteracts.name + " is trying to interact with me");
+            print("<INVENTORY APPEARS> I supply only the finest goods");
+            
+            if (whoInteracts.CompareTag("Player"))
+            {
+                Trader player = whoInteracts.GetComponent<Trader>();
+                Item itemFromPlayer = player.Inventory.GetItems()[0];
+                if (itemFromPlayer)
+                {
+                    player.Trade(itemFromPlayer, this.trader);
+                }
+            }
         }
     }
 }
